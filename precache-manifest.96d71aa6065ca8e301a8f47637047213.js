fcc1236e1ea18f536f4be08374352145d7c96c64
Addison Haight <anhaight@gmail.com>
2026-06-11 22:27:42 -0600

    Updates

---
+++ precache-manifest.96d71aa6065ca8e301a8f47637047213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e48ccd4fccf010223d74dcb9051783",
+    "url": "/coffee-review/index.html"
+  },
+  {
+    "revision": "ec2a1ae9161c9e2f31e5",
+    "url": "/coffee-review/static/css/main.69d37e42.chunk.css"
+  },
+  {
+    "revision": "80c2987ae5f90ad89c53",
+    "url": "/coffee-review/static/js/2.4284f1de.chunk.js"
+  },
+  {
+    "revision": "06daa0885278990682c4688ffd9bb4ec",
+    "url": "/coffee-review/static/js/2.4284f1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2a1ae9161c9e2f31e5",
+    "url": "/coffee-review/static/js/main.b89c6d06.chunk.js"
+  },
+  {
+    "revision": "d5c9ba214def169a2669",
+    "url": "/coffee-review/static/js/runtime-main.083d4f23.js"
+  }
+]);
